8679b2c8b623a47c6d0bb5a7e0bae5a5691b5de0
Yu Yang <yu.yang@cittacorp.com>
2018-09-24 18:44:35 -0400

    test basic

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const electron = require('electron');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+app.on('ready', _=>{
+    mainWindow = new BrowserWindow({
+        height: 400,
+        width: 400
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/countdown.html`);
+
+    mainWindow.on('closed', _=>{
+        mainWindow = null;
+        console.log('main window closed!');
+    });
+});
