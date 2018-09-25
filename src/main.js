e54fd94441dcdf0d4470815751941e6d492ddf77
Yu Yang <yu.yang@cittacorp.com>
2018-09-25 00:37:10 -0400

    test multiple windows

--- src/main.js
+++ src/main.js
@@ -6,23 +6,29 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const ipc = electron.ipcMain;
 
-let mainWindow;
+let windows = [];
 app.on('ready', _=>{
-    mainWindow = new BrowserWindow({
-        height: 400,
-        width: 400
-    });
-
-    mainWindow.loadURL(`file://${__dirname}/countdown.html`);
-
-    mainWindow.on('closed', _=>{
-        mainWindow = null;
-        console.log('main window closed!');
+    [1,2,3].forEach(_=>{
+        let win = new BrowserWindow({
+            height: 400,
+            width: 400
+        });
+    
+        win.loadURL(`file://${__dirname}/countdown.html`);
+    
+        win.on('closed', _=>{
+            // win = null;
+            console.log('window closed!');
+        });
+    
+        windows.push(win);
     });
 });
 
 ipc.on('countdown-start',_=>{
     countdown(count => {
-        mainWindow.webContents.send('countdown', count);
+        windows.forEach(win =>{
+            win.webContents.send('countdown', count);
+        });
     });
 });
